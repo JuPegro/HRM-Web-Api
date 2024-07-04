b8a425bf3bafb0910200db27a17f106f4be0db11
Julio Peguero <jupegro.contact@gmail.com>
2024-07-03 20:01:02 -0400

    Add Roles Middlewares

--- src/middlewares/authJwt.js
+++ src/middlewares/authJwt.js
@@ -5,13 +5,16 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const verifyToken = async (req, res, next) => {
+  // GET TOKEN FROM HEADERS
   let token = req.headers["x_access_token"];
 
   if (!token) return res.status(403).json({ message: "No token provided" });
   try {
+    // VERIFY IS A REALLY TOKEN
     const decoded = jwt.verify(token, SECRET_TOKEN);
     req.id = decoded.id;
 
+    // FOUND USER
     const user = await prisma.user.findUnique({
       where: {
         id: req.id,
@@ -20,10 +23,35 @@ export const verifyToken = async (req, res, next) => {
 
     if (!user) return res.status(404).json({ message: "User not found" });
 
+    // NEXT MIDDLEWARE
     next();
   } catch (error) {
     return res.status(401).json({ message: "Unauthorized!" });
   }
 };
 
-export const isAdmin = async (req, res, next) => {};
+export const isAdmin = async (req, res, next) => {
+  try {
+    // FOUND USER WITH REQ.ID OF VERIFYTOKEN
+    const user = await prisma.user.findUnique({ where: { id: req.id } });
+
+    if (user.role === "ADMIN") next();
+
+    return res.status(403).json({ message: "Require Admin Role" });
+  } catch (error) {
+    return res.status(401).json({ message: "Unauthorized!" });
+  }
+};
+
+export const isModerator = async (req, res, next) => {
+  try {
+    // FOUND USER WITH REQ.ID OF VERIFYTOKEN
+    const user = await prisma.user.findUnique({ where: { id: req.id } });
+
+    if (user.role === "MODERATOR") next();
+
+    return res.status(403).json({ message: "Require Moderator Role" });
+  } catch (error) {
+    return res.status(401).json({ message: "Unauthorized!" });
+  }
+};
