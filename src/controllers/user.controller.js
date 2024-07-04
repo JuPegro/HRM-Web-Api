1d8d334bc3eb166225658a159b2965233f407610
Julio Peguero <jupegro.contact@gmail.com>
2024-07-04 13:06:18 -0400

    CreateUser Already

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,52 @@
+import bcrypt from "bcryptjs";
+import { SALT_ROUNDS } from "../config.js";
 import { PrismaClient } from "@prisma/client";
+import { userValidation } from "../validations/user.validation.js";
 
 const prisma = new PrismaClient();
 
-// CREATE A NEW USER 
-export const createUser = async (req, res, next) => {};
+// CREATE A NEW USER
+export const createUser = async (req, res, next) => {
+  try {
+    // VALIDATE BODY WITH JOI
+    const { error, value } = userValidation.validate(req.body);
+
+    // VALIDATE EMPTY FIELDS
+    if (error) return res.status(400).json({ error: error.details[0].message });
+
+    // FOUND EMAIL ALREADY USE
+    const foundEmail = await prisma.user.findUnique({
+      where: { email: value.email },
+    });
+
+    // CHECK IF EMAIL IS ALREADY IN USE
+    if (foundEmail)
+      return res.status(400).json({ message: "Email is already use!" });
+
+    // ENCRYPT PASSWORD
+    const hashPassword = await bcrypt.hash(value.password, SALT_ROUNDS);
+
+    // CREATE USER IN DATABASE
+    const user = await prisma.user.create({
+      data: {
+        name: value.name,
+        lastname: value.lastname,
+        email: value.email,
+        password: hashPassword,
+      },
+    });
+
+    // OMIT PASSWORD IN RESPONSE
+    const { password: _, ...publicUser } = user;
+
+    // SUCCESSFULLY
+    return res
+      .status(200)
+      .json({ message: "Created user successfully", publicUser });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
 
 // UPDATE AN USER
 export const updateUser = async (req, res, next) => {};
@@ -12,9 +55,7 @@ export const updateUser = async (req, res, next) => {};
 export const deleteUser = async (req, res, next) => {};
 
 // GET ALL USER
-export const getUsers = async (req, res, next) => {
-    res.send('ESTA FUNCIONANDO MMGVASO')
-};
+export const getUsers = async (req, res, next) => {};
 
 // GET AN USER
 export const getUserById = async (req, res, next) => {};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,9 +4,9 @@ const router = Router();
 //  IMPORT USER CONTROLLER
 import * as userCtrl from "../controllers/user.controller.js";
 
+router.post("/user", userCtrl.createUser); // CREATE NEW USER
 router.get("/user", userCtrl.getUsers); // GET ALL USERS
 router.get("/user/:id", userCtrl.getUserById); // GET AN USERS
-router.post("/user", userCtrl.createUser); // CREATE NEW USER
 router.put("/user/:id", userCtrl.updateUser); // UPDATE AN USER
 router.put("/user/:id", userCtrl.changeStatusUser); // CHANGE STATUS OF USER
 router.delete("/user/:id", userCtrl.deleteUser); // DELETE AN USER
