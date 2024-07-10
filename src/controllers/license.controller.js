20b98a203ff5e17792996c1db9f110a64c7ba5c4
Julio Peguero <jupegro.contact@gmail.com>
2024-07-09 23:04:43 -0400

    Complete License

---
+++ src/controllers/license.controller.js
@@ -0,0 +1,156 @@
+import { PrismaClient } from "@prisma/client";
+import {
+  licenseValidation,
+  statusValidation,
+} from "../validations/license.validation.js";
+
+const prisma = new PrismaClient();
+
+// CREATE A NEW LICENSE
+export const createLicense = async (req, res, nex) => {
+  try {
+    const { error, value } = licenseValidation.validate(req.body);
+
+    // VALIDATE EMPTY FIELDS
+    if (error) return res.status(400).json({ error: error.details[0].message });
+
+    const license = await prisma.license.create({
+      data: value,
+    });
+
+    return res
+      .status(201)
+      .json({ message: "Created license successfully", license });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+// GET ALL LICENSE
+export const getLicense = async (req, res, nex) => {
+  try {
+    const licenses = await prisma.license.findMany();
+
+    if (licenses.length === 0)
+      return res.status(404).json({ message: "Licenses not found!" });
+
+    return res.status(200).json({ licenses: licenses });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// GET A ONE LICENSE
+export const getLicenseById = async (req, res, nex) => {
+  try {
+    const { id } = req.params;
+
+    // CHECK PROVIDED ID
+    if (!id) return res.status(400).json({ message: "Id not provided" });
+
+    const license = await prisma.license.findUnique({
+      where: { id: id },
+    });
+
+    // CHECK USER FOUND
+    if (!license) return res.status(404).json({ message: "License not found!" });
+
+    return res.status(200).json({ license: license });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// UPDATE A LICENSE
+export const updateLicense = async (req, res, nex) => {
+  try {
+    // GET LICENSE ID
+    const { id } = req.params;
+
+    // CHECK ID PROVIDED
+    if (!id) return res.status(400).json({ message: "Id not provided" });
+
+    // VALIDATE BODY WITH JOI
+    const { error, value } = licenseValidation.validate(req.body);
+
+    // VALIDATE EMPTY FIELDS
+    if (error) return res.status(400).json({ error: error.details[0].message });
+
+    // UPDATE LICENSE IN DATABASE
+    const license = await prisma.license.update({
+      where: {
+        id: id,
+      },
+      data: value,
+    });
+
+    return res
+      .status(202)
+      .json({ message: "Updated license sucessfully", license });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// DELETE A LICENSE
+export const deleteLicense = async (req, res, nex) => {
+  try {
+    const { id } = req.params;
+
+    // CHECK ID PROVIDED
+    if (!id) return res.status(400).json({ message: "Id not provided!" });
+
+    // FIND LICENSE
+    const license = await prisma.license.findUnique({
+      where: { id: id },
+    });
+
+    if (!license) return res.status(404).json({ message: "License not found!" });
+
+    // DELETE LICENSE IN DATABASE
+    await prisma.license.delete({
+      where: { id: id },
+    });
+
+    return res.status(200).json({ message: "Deleted license successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// CHANGE STATUS LICENSE
+export const changeStatusLicense = async (req, res, nex) => {
+  try {
+    const { id } = req.params;
+
+    const { error, value } = statusValidation.validate(req.body);
+
+    console.log(error);
+
+    // CHECK PROVIDED ID
+    if (!id) return res.status(400).json({ message: "Id not provided" });
+
+    // EMPTY STATUS
+    if (error) return res.status(400).json({ error: error.details[0].message });
+
+    // GET DATA LICENSE
+    const license = await prisma.license.findUnique({ where: { id: id } });
+
+    // IF NOT FOUND
+    if (!license) return res.status(404).json({ message: "License not found!" });
+
+    // CHANGE STATUS IN DATABASE
+    const change = await prisma.license.update({
+      where: { id: id },
+      data: {
+        status: value.status,
+      },
+    });
+
+    return res
+      .status(200)
+      .json({ message: `Status change to ${change.status}` });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
--- src/routes/license.routes.js
+++ src/routes/license.routes.js
@@ -2,5 +2,22 @@ import { Router } from "express";
 
 const router = Router();
 
+// IMPORT JWT CONTROLLER
+import * as jwtCtrl from "../middlewares/authJwt.js";
+
+// IMPORT LICENSE CONTROLLER
+import * as licenseCtrl from "../controllers/license.controller.js";
+
+router.post('/license', [jwtCtrl.verifyToken, jwtCtrl.isModerator], licenseCtrl.createLicense) // CREATE NEW LICENSE
+
+router.get("/license", jwtCtrl.verifyToken, licenseCtrl.getLicense) // GET ALL LICENSE
+
+router.get("/license/:id", jwtCtrl.verifyToken, licenseCtrl.getLicenseById) // GET A ONE LICENSE
+
+router.put('/license/:id', [jwtCtrl.verifyToken, jwtCtrl.isModerator], licenseCtrl.updateLicense) // UPDATE A LICENSE
+
+router.put('/license/:id/status', [jwtCtrl.verifyToken], jwtCtrl.isAdmin, licenseCtrl.changeStatusLicense) // CHANGE STATUS
+
+router.delete('/license/:id', [jwtCtrl.verifyToken, jwtCtrl.isAdmin], licenseCtrl.deleteLicense) // DELETE A LICENSE
 
 export default router;
