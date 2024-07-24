843339baddeaf9c751acb796f4c7ecf0f6b44420
Julio Peguero <jupegro.contact@gmail.com>
2024-07-23 23:40:44 -0400

    Auth Validation

---
+++ src/validations/auth.validation.js
@@ -0,0 +1,23 @@
+import Joi from "joi";
+
+// DEFINE THE VALIDATION SCHEME
+export const authValidation = Joi.object({
+  email: Joi.string().email().required().messages({
+    "string.base": "Email must be a string",
+    "string.empty": "Email is required",
+    "string.email": "Email must be a valid email address",
+    "any.required": "Email is required",
+  }),
+  password: Joi.string()
+    .min(8)
+    .pattern(new RegExp("^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$"))
+    .required()
+    .messages({
+      "string.base": "Password must be a string",
+      "string.empty": "Password is required",
+      "string.min": "Password must be at least 8 characters long",
+      "string.pattern.base":
+        "Password must contain at least one letter, one number, and one special character",
+      "any.required": "Password is required",
+    }),
+});
